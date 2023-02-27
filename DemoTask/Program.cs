b8413d581f5690013194cb4d1bccadbdbecc47f6
Mikhail Korsun <mr.reduction@gmail.com>
2023-02-27 18:46:21 +0300

    DemoTask and Random

---
+++ DemoTask/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+//Например:
+//78 -> 8
+//12-> 2
+//85 -> 8
+
+Random rand= new Random();
+int number = rand.Next(10,100);
+int max = number % 10;
+Console.WriteLine(number);
+if (max > ((number-max) / 10)) 
+    Console.WriteLine($"Наибольшая цифра: {max}");
+    else
+    Console.WriteLine($"Наибольшая цифра: {(number-max) / 10}");
